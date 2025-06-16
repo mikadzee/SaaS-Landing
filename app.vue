c3336e05bcba0049071768982b150f60766c8978
Mikadzee <asansabatyrov@gmail.com>
2025-06-16 08:59:23 +0300

    feat: добавление useHead

--- app.vue
+++ app.vue
@@ -14,6 +14,9 @@
 
 <script setup>
   useHead({
-    
+    title: 'SaaS Landing Page',
+    meta: [
+      { name: 'description', content: 'This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com' }
+    ]
   })
 </script>
